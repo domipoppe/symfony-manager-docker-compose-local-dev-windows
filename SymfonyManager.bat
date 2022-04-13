bcf17501c636ff0fab4fbc04394ab86b9277daaa
Dominic Poppe <d.poppe@idiox.io>
2022-04-13 15:07:48 +0200

    Update
    Remove Axigen Mailserver
    
    Adds PHP 7.4 to composer
    
    Adds option for batch file to not install components as Mailer, Finder...
    
    Fixup template files
    
    Update gitlab CI
    
    Update PHP container

--- SymfonyManager.bat
+++ SymfonyManager.bat
@@ -16,7 +16,6 @@ echo NGINX and PHP-FPM as web server
 echo MariaDB as database
 echo REDIS for queues/cache
 echo PHPMYADMIN for GUI database management
-echo Axigen Mailserver
 echo SFTP server
 
 echo.
@@ -25,6 +24,11 @@ set /p projectname=Enter the project name:
 echo.
 
 echo Project %projectname% is being created now...
+echo.
+echo.
+
+echo Would you like to install components (Mailer, Finder, Translator...)?
+set /p input=yes / no: 
 echo.
 echo.
 
@@ -38,60 +42,61 @@ echo.
 call symfony new --full %projectname%
 cd %projectname%
 
+echo Require PHP...
+echo.
+call composer require php ">=7.4"
+echo.
+
 echo Install console component...
 echo.
 call composer require symfony/console
 echo.
 
-echo Install nelmio security component...
+echo Install nelmio & roave security component...
 echo.
 call composer require nelmio/security-bundle
+call composer require --dev roave/security-advisories:dev-latest
 echo.
 
-echo Install filesystem component...
-echo.
-call composer require symfony/filesystem
-echo.
+if %input%==yes echo Install filesystem component...
+if %input%==yes echo.
+if %input%==yes call composer require symfony/filesystem
+if %input%==yes echo.
 
-echo Install sentry component...
-echo.
-call composer require sentry/sentry-symfony
-echo.
+if %input%==yes echo Install sentry component...
+if %input%==yes echo.
+if %input%==yes call composer require sentry/sentry-symfony
+if %input%==yes echo.
 
 echo Install KnpTime component...
 echo.
 call composer require knplabs/knp-time-bundle
 echo.
 
-echo Install finder component...
-echo.
-call composer require symfony/finder
-echo.
-
-echo Install IMAP component...
-echo.
-call composer require secit-pl/imap-bundle
-echo.
+if %input%==yes echo Install finder component...
+if %input%==yes echo.
+if %input%==yes call composer require symfony/finder
+if %input%==yes echo.
 
 echo Install form component...
 echo.
 call composer require symfony/form
 echo.
 
-echo Install http-client component...
-echo.
-call composer require symfony/http-client
-echo.
+if %input%==yes echo Install http-client component...
+if %input%==yes echo.
+if %input%==yes call composer require symfony/http-client
+if %input%==yes echo.
 
-echo Install mailer component...
-echo.
-call composer require symfony/mailer
-echo.
+if %input%==yes echo Install mailer component...
+if %input%==yes echo.
+if %input%==yes call composer require symfony/mailer
+if %input%==yes echo.
 
-echo Install notifier component...
-echo.
-call composer require symfony/notifier
-echo.
+if %input%==yes echo Install notifier component...
+if %input%==yes echo.
+if %input%==yes call composer require symfony/notifier
+if %input%==yes echo.
 
 echo Install uuid component...
 echo.
@@ -105,7 +110,7 @@ echo.
 
 echo Install var-dumper component...
 echo.
-call composer require symfony/var-dumper
+call composer require --dev symfony/var-dumper
 echo.
 
 echo Install debug component...
@@ -113,10 +118,10 @@ echo.
 call composer require symfony/debug-bundle
 echo.
 
-echo Install translator component...
-echo.
-call composer require symfony/translation
-echo.
+if %input%==yes echo Install translator component...
+if %input%==yes echo.
+if %input%==yes call composer require symfony/translation
+if %input%==yes echo.
 
 echo Install PHPStan component...
 echo.
@@ -133,10 +138,10 @@ echo.
 call composer require --dev phpstan/phpstan-symfony
 echo.
 
-echo Install DOMPDF component...
-echo.
-call composer require dompdf/dompdf
-echo.
+if %input%==yes echo Install DOMPDF component...
+if %input%==yes echo.
+if %input%==yes call composer require dompdf/dompdf
+if %input%==yes echo.
 
 echo Install PHPUnit component...
 echo.
@@ -148,19 +153,19 @@ echo.
 call composer require symfony/cache
 echo.
 
-echo Install encrypt-decrypt doctrine component...
-echo.
-call composer require michaeldegroot/doctrine-encrypt-bundle
-echo.
+if %input%==yes echo Install encrypt-decrypt doctrine component...
+if %input%==yes echo.
+if %input%==yes call composer require michaeldegroot/doctrine-encrypt-bundle
+if %input%==yes echo.
 
-echo Install UX components...
-echo.
-call composer require symfony/ux-chartjs
-call composer require symfony/ux-cropperjs
-call composer require symfony/ux-dropzone
-call composer require symfony/ux-lazy-image
-call composer require symfony/ux-swup
-echo.
+if %input%==yes echo Install UX components...
+if %input%==yes echo.
+if %input%==yes call composer require symfony/ux-chartjs
+if %input%==yes call composer require symfony/ux-cropperjs
+if %input%==yes call composer require symfony/ux-dropzone
+if %input%==yes call composer require symfony/ux-lazy-image
+if %input%==yes call composer require symfony/ux-swup
+if %input%==yes echo.
 
 echo Install YARN components...
 echo.
@@ -168,30 +173,21 @@ call yarn install --force
 call yarn encore dev
 echo.
 
-echo Install VichUploaderBundle...
-echo.
-call composer require vich/uploader-bundle
-echo.
-
-echo Install NelmioApiDocBundle...
-echo.
-call composer require nelmio/api-doc-bundle
-echo.
-
 echo Install Schedule Bundle...
 echo.
 call composer require zenstruck/schedule-bundle
 echo.
 
-echo Install Translation Bundle...
-echo.
-call composer require php-translation/symfony-bundle
-echo.
+if %input%==yes echo Install Doctrine Enum Bundle...
+if %input%==yes echo.
+if %input%==yes call composer require fresh/doctrine-enum-bundle
+if %input%==yes echo.
 
-echo Install Doctrine Enum Bundle...
-echo.
-call composer require fresh/doctrine-enum-bundle
-echo.
+if %input%==yes echo Install Doctrine Extensions...
+if %input%==yes echo.
+if %input%==yes call composer require gedmo/doctrine-extensions
+if %input%==yes call composer require stof/doctrine-extensions-bundle
+if %input%==yes echo.
 
 echo Install Twig Time Extension...
 echo.
@@ -213,9 +209,11 @@ echo.
 
 echo Rename docker containers to project name...
 echo.
-powershell -Command "(gc docker-compose.yaml) -replace 'PROJECTNAME', '%projectname%' | Out-File -encoding UTF8 docker-compose.yaml"
-powershell -Command "(gc .env) -replace 'db:3306', '%projectname%_db:3306' | Out-File -encoding UTF8 .env"
-powershell -Command "(gc .gitlab-ci.yml) -replace 'docker exec php', 'docker exec %projectname%_php' | Out-File -encoding UTF8 .gitlab-ci.yml"
+powershell -Command "(gc docker-compose.yaml) -replace 'PROJECTNAME', '%projectname%' | Out-File -encoding ASCII docker-compose.yaml"
+powershell -Command "(gc .env) -replace 'db:3306', '%projectname%_db:3306' | Out-File -encoding ASCII .env"
+powershell -Command "(gc .gitlab-ci.yml) -replace 'docker exec php', 'docker exec %projectname%_php' | Out-File -encoding ASCII .gitlab-ci.yml"
+powershell -Command "(gc docker\nginx\conf.d\default.conf) -replace 'server php:9000;', 'server %projectname%_php:9000;' | Out-File -encoding ASCII docker\nginx\conf.d\default.conf"
+powershell -Command "(gc composer.json) -replace '>=7.2.5', '>=7.4.0' | Out-File -encoding ASCII composer.json"
 echo.
 
 echo.
